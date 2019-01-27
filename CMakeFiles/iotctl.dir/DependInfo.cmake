206d202210ce41ad0edfd66c94cf0fc27cce976d
Eddy Scott <eddy.scott88@gmail.com>
2019-01-27 11:25:24 -0500

    Default project with working desktop debugging

---
+++ CMakeFiles/iotctl.dir/DependInfo.cmake
@@ -0,0 +1,41 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/eddy/projects/iotctl/iotctl_autogen/EWIEGA46WW/qrc_qml.cpp" "/home/eddy/projects/iotctl/CMakeFiles/iotctl.dir/iotctl_autogen/EWIEGA46WW/qrc_qml.cpp.o"
+  "/home/eddy/projects/iotctl/iotctl_autogen/mocs_compilation.cpp" "/home/eddy/projects/iotctl/CMakeFiles/iotctl.dir/iotctl_autogen/mocs_compilation.cpp.o"
+  "/home/eddy/projects/iotctl/main.cpp" "/home/eddy/projects/iotctl/CMakeFiles/iotctl.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_NO_DEBUG"
+  "QT_QML_LIB"
+  "QT_QUICK_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "iotctl_autogen/include"
+  "/home/eddy/Qt5.12.0/5.12.0/android_arm64_v8a/include"
+  "/home/eddy/Qt5.12.0/5.12.0/android_arm64_v8a/include/QtCore"
+  "/home/eddy/Qt5.12.0/5.12.0/android_arm64_v8a/./mkspecs/android-clang"
+  "/home/eddy/Qt5.12.0/5.12.0/android_arm64_v8a/include/QtQuick"
+  "/home/eddy/Qt5.12.0/5.12.0/android_arm64_v8a/include/QtQml"
+  "/home/eddy/Qt5.12.0/5.12.0/android_arm64_v8a/include/QtNetwork"
+  "/home/eddy/Qt5.12.0/5.12.0/android_arm64_v8a/include/QtGui"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/iotctl.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for iotctl.
+# This may be replaced when dependencies are built.
